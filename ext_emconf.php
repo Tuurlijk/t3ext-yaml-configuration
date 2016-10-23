32a5c97547b7c38f46b8ac9fa3d6543c7850b255
Josef Glatz <j.glatz@supseven.at>
2016-10-23 20:53:39 +0200

    [TASK] Update version constraints for TYPO3 8.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '1.0.10',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.0-7.99.99',
+            'typo3' => '6.2.0-8.4.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
