Tuurlijk/t3ext-yaml-configuration

commit 5a00ab24f2b779ab3fedb0bd1689f8b8bf737a69

[FEATURE] Re-enable option to match usergroup by uid when exporting and importing be_users (#24)

Classes/Command/ImportCommandController.php
@@ -43,11 +43,13 @@ class ImportCommandController extends AbstractCommandController
      * @since 1.0.0
      *
      * @param string $matchFields Comma separated list of fields used to match configurations to database records. Default: **username**
+     * @param bool $beUserMatchGroupByTitle Match be_group settings in be_users.usergroup by title or uid;
+     *             if true be_groups.title is used. Default: **false**
      * @param string $file Path to the yml file you wish to import. If none is given, all yml files in directories named 'Configuration' will be parsed
      */
-    public function backendUsersCommand($matchFields = 'username', $file = null)
+    public function backendUsersCommand($matchFields = 'username', $beUserMatchGroupByTitle = false, $file = null)
     {
-        $this->importData('be_users', $matchFields, $file);
+        $this->importData('be_users', $matchFields, $beUserMatchGroupByTitle, $file);
     }
 
     /**
@@ -112,11 +114,13 @@ class ImportCommandController extends AbstractCommandController
      *
      * @since 1.0.0
      *
-     * @param $table
+     * @param string $table
      * @param string $matchFields Comma separated list of fields used to match configurations to database records.
+     * @param bool $beUserMatchGroupByTitle Match be_group settings in be_users.usergroup by title or uid;
+     *             if true be_groups.title is used. Default: **false**
      * @param string $file Path to the yml file you wish to import. If none is given, all yml files in directories named 'Configuration' will be parsed
      */
-    protected function importData($table, $matchFields, $file = null)
+    protected function importData($table, $matchFields, $beUserMatchGroupByTitle = false, $file = null)
     {
         $table = preg_replace('/[^a-z0-9_]/', '', $table);
         $matchFields = explode(',', preg_replace('/[^a-z0-9_,]/', '', $matchFields));
@@ -153,7 +157,7 @@ class ImportCommandController extends AbstractCommandController
                 if ($row) {
                     $this->successMessage('Found existing ' . $table . ' record by matchfields: ' . $matchClause);
                     $this->message('Updating . . .');
-                    if(isset($record['usergroup'])) {
+                    if (isset($record['usergroup']) && $beUserMatchGroupByTitle) {
                         $record['usergroup'] = $this->convertUsergroupNamesToUid($record);
                     }
                     $record = $this->updateTimeFields($record, $columnNames, array('tstamp'));
@@ -204,4 +208,5 @@ class ImportCommandController extends AbstractCommandController
 
         return $commaSepratedGroupUids;
     }
+
 }

Classes/Command/ExportCommandController.php
@@ -36,6 +36,7 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
  */
 class ExportCommandController extends AbstractCommandController
 {
+
     /**
      * Export be_users table to yml file
      *
@@ -46,15 +47,18 @@ class ExportCommandController extends AbstractCommandController
      * @param bool $includeDeleted Export deleted records. Default: **false**
      * @param bool $includeHidden Export hidden/disable records. Default: **false**
      * @param integer $indentLevel Indent level to make yaml file human readable. Default: **2**
+     * @param bool $beUserMatchGroupByTitle Match be_group settings in be_users.usergroup by title or uid;
+     *             if true be_groups.title is used. Default: **false**
      */
     public function backendUsersCommand(
         $file = null,
         $skipColumns = 'crdate,lastlogin,tstamp,uc',
         $includeDeleted = false,
         $includeHidden = false,
-        $indentLevel = 2
+        $indentLevel = 2,
+        $beUserMatchGroupByTitle = false
     ) {
-        $this->exportTable('be_users', $file, $skipColumns, $includeDeleted, $includeHidden, $indentLevel);
+        $this->exportTable('be_users', $file, $skipColumns, $includeDeleted, $includeHidden, $indentLevel, $beUserMatchGroupByTitle);
     }
     /**
      * Export be_groups table to yml file
@@ -153,6 +157,8 @@ class ExportCommandController extends AbstractCommandController
      * @param bool $includeDeleted Export deleted records. Default: **false**
      * @param bool $includeHidden Export hidden/disable records. Default: **false**
      * @param integer $indentLevel Indent level to make yaml file human readable. Default: **2**
+     * @param bool $beUserMatchGroupByTitle Match be_group settings in be_users.usergroup by title or uid;
+     *             if true be_groups.title is used. Default: **false**
      *
      * @return void
      */
@@ -162,7 +168,8 @@ class ExportCommandController extends AbstractCommandController
         $skipColumns = 'crdate,lastlogin,tstamp,uc',
         $includeDeleted = false,
         $includeHidden = false,
-        $indentLevel = 2
+        $indentLevel = 2,
+        $beUserMatchGroupByTitle = false
     ) {
         $table = preg_replace('/[^a-z0-9_]/', '', $table);
         $skipColumns = explode(',', $skipColumns);
@@ -208,7 +215,7 @@ class ExportCommandController extends AbstractCommandController
 
                     // Do not update usergroups by UID when exporting to other systems
                     // UID maybe different for the same usergroup name
-                    if ($table == 'be_users' && $column == 'usergroup' && $value) {
+                    if ($beUserMatchGroupByTitle && $table == 'be_users' && $column == 'usergroup' && $value) {
                         $usergroups = $this->databaseConnection->exec_SELECTgetRows('title', 'be_groups', 'uid IN (' . $value . ')');
                         // @todo Currently the sorting of usergroups in the original records is ignored when exporting usergroups
                         $usergroupsTitles = [];

commit 465d687a58c967632c6de4c913851c85e6da6cd3

[BUGFIX] Fix regression which was introduced with  (#24)
Move previously introduced importData argument "$beUserMatchGroupByTitle" to last position.
This fix is necessary to re-enable the file argument when importing data with e.g. table import
command.

Fixes: #26

Classes/Command/ImportCommandController.php
@@ -49,7 +49,7 @@ class ImportCommandController extends AbstractCommandController
      */
     public function backendUsersCommand($matchFields = 'username', $beUserMatchGroupByTitle = false, $file = null)
     {
-        $this->importData('be_users', $matchFields, $beUserMatchGroupByTitle, $file);
+        $this->importData('be_users', $matchFields, $file, $beUserMatchGroupByTitle);
     }
 
     /**
@@ -116,11 +116,11 @@ class ImportCommandController extends AbstractCommandController
      *
      * @param string $table
      * @param string $matchFields Comma separated list of fields used to match configurations to database records.
+     * @param string $file Path to the yml file you wish to import. If none is given, all yml files in directories named 'Configuration' will be parsed
      * @param bool $beUserMatchGroupByTitle Match be_group settings in be_users.usergroup by title or uid;
      *             if true be_groups.title is used. Default: **false**
-     * @param string $file Path to the yml file you wish to import. If none is given, all yml files in directories named 'Configuration' will be parsed
      */
-    protected function importData($table, $matchFields, $beUserMatchGroupByTitle = false, $file = null)
+    protected function importData($table, $matchFields, $file = null, $beUserMatchGroupByTitle = false)
     {
         $table = preg_replace('/[^a-z0-9_]/', '', $table);
         $matchFields = explode(',', preg_replace('/[^a-z0-9_,]/', '', $matchFields));